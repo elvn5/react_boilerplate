eeaa51a8c62ae138190b17b7fd96e826d2f17f3d
ca11_911 <zetxc996@gmail.com>
2024-04-22 10:20:15 +0600

    fix: deleted some unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import { DevTools } from 'jotai-devtools';
-import 'jotai-devtools/styles.css'
 
 function App() {
   return (
-    <div className="App">
-     <DevTools position="bottom-right" theme="dark"/>
+    <div>
     </div>
   );
 }

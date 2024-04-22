be6821c84c23c0546ec703844801d2b3d7e6fcb0
ca11_911 <zetxc996@gmail.com>
2024-04-22 10:18:51 +0600

    feat: updated boilerplate with styles system and with some new folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { DevTools } from 'jotai-devtools';
 import 'jotai-devtools/styles.css'
-import { Button } from '@components/Button';
 
 function App() {
   return (
     <div className="App">
-     <Button/>
      <DevTools position="bottom-right" theme="dark"/>
     </div>
   );
--- src/components/Button/index.tsx
+++
@@ -1,3 +0,0 @@
-import styles from './Button.module.scss';
-
-export const Button = (): JSX.Element => <button className={styles.button}>Ok</button>;

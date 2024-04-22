47571b57d8947e588748fd21cacedf91efb154d0
ca11_911 <zetxc996@gmail.com>
2024-04-22 10:35:04 +0600

    fix: fixed deps

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // eslint-disable-next-line quotes
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
